2ccbbfd382635dd547367ebf874acbddb49595c0
vonsofh <vonsofh@gmail.com>
2026-03-02 13:30:04 +0700

    chore: add custom domain vonsofh.cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vonsofh.github.io',
+  site: 'https://vonsofh.cv',
   vite: {
     plugins: [tailwindcss()],
   },
